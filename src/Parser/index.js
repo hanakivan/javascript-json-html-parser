b252089dbb90b12468a5c0641a71e4445a2b9a46
Ivan Hanak <hanakivan@gmail.com>
2020-08-02 18:21:03 +0200

    Tree walker added

--- src/Parser/index.js
+++ src/Parser/index.js
@@ -1,56 +1,104 @@
 const BLOCK_ELEMENTS = [
     "paragraph",
     "heading",
+    "preformatted",
     "list",
-    "preformatted"
 ];
 
 const INLINE_ELEMENTS = [
-    ""
+    "em",
+    "strong",
+    "b",
+    "i",
+    "anchor",
+    "span",
+    "cite",
+    "code",
+    "key",
+    "break"
 ];
 
-export class Parser {
+export const isBlockElement = (elementName) => {
+    return BLOCK_ELEMENTS.includes(elementName);
+};
 
-    html = null;
+export const isInlineElement = (elementName) => {
+    return INLINE_ELEMENTS.includes(elementName);
+};
 
-    constructor(htmlCode) {
-        this.html = htmlCode;
-    }
-
-    getHtml() {
-        return this.html;
-    }
-}
+const isTextNode = (elementName) => {
+    return elementName === "text";
+};
 
 export const parseChildren = (block, children) => {
     children.forEach(kid => {
-        if(kid.type === "text") {
+        if(isTextNode(kid.type)) {
             const kiddo = document.createTextNode(kid.text);
 
             block.appendChild(kiddo);
-        } else if (["em", "strong", "b", "i", "span", "cite", "code", "key", "break"].includes(kid.type)) {
-            const kiddo = document.createElement(getElementTagNameForName(kid.type));
+        } else if (isInlineElement(kid.type)) {
+            const kiddo = document.createElement(getElementTagNameForInlineElementName(kid.type));
+
+            if(kiddo.tagName.toLowerCase() === "a") {
+                kiddo.href = kid.href;
+            }
 
             if(kid.children) {
-                kid.children.forEach(subKid => {
-                    if(subKid.type === "text") {
-                        const subKiddo = document.createTextNode(subKid.text);
-
-                        kiddo.appendChild(subKiddo);
-                    }
-                });
+                parseChildren(kiddo, kid.children);
             }
 
             block.appendChild(kiddo);
+        } else if (kid.type === "listitem") {
+            const listItem = document.createElement(`li`);
+
+            parseChildren(listItem, kid.children);
+
+            block.appendChild(listItem);
+        } else {
+            console.warn("Parsing an unknown element tyoe", kid);
         }
     });
 };
 
-const getElementTagNameForName = (elementName) => {
+const getElementTagNameForInlineElementName = (elementName) => {
     switch(elementName) {
         case "break":
             return "br";
+        case "anchor":
+            return "a";
     }
 
     return elementName;
+};
+
+export const createElementForElementName = (elementName, options = {}) => {
+    let block = null;
+
+    switch(elementName) {
+        case "paragraph":
+            block = document.createElement("p");
+            break;
+        case "heading":
+            const level = options.level;
+            block = document.createElement(`h${level}`);
+            break;
+        case "preformatted":
+            block = document.createElement("pre");
+            break;
+        case "list":
+            const order = options.order === "ordered" ? "ol" : "ul";
+            block = document.createElement(order);
+            break;
+        default:
+            console.warn("unknown element name", elementName);
+            break;
+    }
+
+    if(block) {
+        if(typeof options["id"] !== "undefined") {
+            block.id = options["id"];
+        }
+    }
+
+    return block;
 };
